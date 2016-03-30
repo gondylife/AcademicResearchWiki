f17e7722739caadab3a014f6b8d8739fe2c595b0
Godswill Okwara <gondy4life@gmail.com>
2016-03-30 10:51:53 +0100

    topic schema created

---
+++ Server/topic.js
@@ -0,0 +1,36 @@
+var mongoose = require('mongoose');
+var Category = require('/category');
+
+var topicSchema = {
+	name: { type: String, required: true },
+	intro: { type: String, required: true },
+	content: [{
+		title: { type: String, required: true },
+		body: { type: String, required: true },
+		number: { type: Number, required: true },
+		parent: { type: Number, required: true }
+	}],
+	images: [{
+		link: { type: String, match: /^http:\/\//i },
+		content: { type: Number }
+	}],
+	meta: {
+		creation: {
+			by: { type: String, required: true },
+			date: { type: Date, required: true }
+		},
+		last_edited: {
+			by: { type: String, required: true },
+			date: { type: Date, required: true }
+		},
+		votes: {
+			upvotes: { type: Number, required: true },
+			downvotes: { type: Number, required: true }
+		}
+	},
+	bibliography: [{ type: String, required: true }],
+	category: Category.categorySchema
+};
+
+module.exports = new mongoose.Schema(topicSchema);
+module.exports.topicSchema = topicSchema;
