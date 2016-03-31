27806b65de550a15672f8bfeb28a6273db18365a
Godswill Okwara <gondy4life@gmail.com>
2016-03-31 10:50:00 +0100

    added topic model to models and wrote a factory to register all services to ensure DRYness

--- Server/models.js
+++ Server/models.js
@@ -1,16 +1,25 @@
 var mongoose = require('mongoose');
+var _ = require('underscore');
 
 module.exports = function(wagner) {
   	mongoose.connect('mongodb://localhost:27017/test');
 
   	var Category =
     	mongoose.model('Category', require('./category'), 'categories');
+  	var Topic =
+    	mongoose.model('Topic', require('./topic'), 'topics');
 
-	wagner.factory('Category', function() {
-		return Category;
-	});
+  	var models = {
+    	Category: Category,
+    	Topic: Topic
+  	};
 
-	return {
-		Category: Category
-	};
+  	// To ensure DRY-ness, register factories in a loop
+  	_.each(models, function(value, key) {
+    	wagner.factory(key, function() {
+      		return value;
+    	});
+  	});
+
+  	return models;
 };
